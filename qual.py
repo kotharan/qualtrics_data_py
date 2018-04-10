a547fcb02d53fbf8f7713049673e01cf7b673d58
Kyle Magness <36307365+magnessk@users.noreply.github.com>
2018-04-10 00:57:44 -0700

    expanded qual.py, added variables
    so the qual.py still doesnt do anything useful but I added a cnfig file (variables.py) that is read on start and has extra variables such as some starting columns. i only added the column numbers they'll use for the final spreadsheet, but we can make them lists and have them also reference columns in the first spreadsheet. ill do that next time I guess.

--- qual.py
+++ qual.py
@@ -1,9 +1,16 @@
 import csv
+from time import sleep
+from variables import *  #I offloaded all our column nmaes into another script file
+
+print(checkvariable)
 
 with open('outdoor.csv',newline='') as f:
     reader = csv.reader(f)
     row1 = next(reader) # gets the first line
+    row2 = next(reader) # the second
      # now do something here
     # if first row is the header, then you can do one more next() to get the next row:
     # row2 = next(f)
-print(row1)
+for k in range(len(row2)):
+    print(row2[k]) #fyi python uses brackets for list indices
+    sleep(1)
---
+++ variables.py
@@ -0,0 +1,12 @@
+#using this, we can just edit this file instead of haphazardly editing the main
+checkvariable='check variable is good'
+ESDname=0
+SCHname=1
+ODSname=2
+Location=3
+grade5students_attending=4
+grade6students_attending=5
+program_days=6
+program_nights=7
+per_student_cost=8   #we should add more columns for extra student costs and stuff
+per_student_SCH_EST_cost=9 #school estimated cost
