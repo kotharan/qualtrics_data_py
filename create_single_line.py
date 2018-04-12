7f59d13878b82275306a20bc902be20571e7e4d4
Kale <thekaleman@users.noreply.github.com>
2018-04-12 14:17:53 -0700

    trying to get atom to work with git. this file is only partial

--- create_single_line.py
+++ create_single_line.py
@@ -1,11 +1,16 @@
 
+def allocatelist(times):
+    var=[]
+    for k in range(times):
+        line.append('_')
+        return var
 
 
+def scrap_data_into_list(fullstring):
 
-
-def getschoolinfo(fullstring):
-    line=['_'*30]
+    list=allocatelist(60)
     line[ESDname[1]]=fullstring[ESDname[0]]
     line[SCHname[1]]=fullstring[SCHname[0]]
     line[ODS_provider_name[1]]=fullstring[ODS_provider_name[0]]
     line[ODSotheroption[1]]=fullstring[ODSotheroption[0]]
+    line[per_student_SCH_EST_cost[1]]=fullstring[per_student_SCH_EST_cost[0]]
