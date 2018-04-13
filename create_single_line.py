e8d368bb4983e18752b703fac5549624124d80b3
Kale <thekaleman@users.noreply.github.com>
2018-04-13 11:52:16 -0700

    debugged the reader script.
    just input a number greater than 4 when it asks and it will print out the relevant info from each school on that row. I am very proud.

--- create_single_line.py
+++ create_single_line.py
@@ -6,19 +6,52 @@ def allocatelist(var,times):  #this just allocates the list
         var.append('_')
     return var
 
+
+def get_num_of_schools_row(fullstring):
+    try:
+        if fullstring[SCHname[0]] != '':
+            num=1
+        else:
+            num=0
+            #separation distance between schools is 230 cells
+    except IndexError:
+        num=0
+        run=0
+
+
+    sep=23
+    run=1
+    while run != 0:
+        try:
+            if fullstring[(SCHname[0]+run*sep)] !='':
+                run=run+1
+                num=run
+            else:
+                run=0
+        except IndexError:
+            num=1
+            run=0
+    return num
+
+
+
+
+
 #this tells us the total per student cost, and works for any school in a row
 def generate_cost(fullstring,ODS):
     perSTtotal= fullstring[ODS]+fullstring[ODS+1]+fullstring[ODS+2]+fullstring[ODS+3]
     return perSTtotal
 
+#This function will get the number of schools in a row
+
+
 
 #this function scrapes all the data for a given school in a list
-def scrape_data_into_list(fullstring,SCHnamelocation):
+def scrape_data_into_list(fullstring,schnum):
 #this line allows this function to work for any school in a row
-    diff=SCHnamelocation-SCHname[0]
-    print(diff)
+    diff=(schnum-1)*23
     var=[]
-    line=allocatelist(var,6)
+    line=allocatelist(var,11)
     line[ESDname[1]]=fullstring[ESDname[0]] #one ESD per line so this shouldn't change
     line[SCHname[1]]=fullstring[(SCHname[0]+diff)] #school name
     line[ODS_provider_name[1]]=fullstring[ODS_provider_name[0]+diff] #ods name
@@ -27,5 +60,7 @@ def scrape_data_into_list(fullstring,SCHnamelocation):
     else:
         line[ODSotheroption[1]]=fullstring[ODSotheroption[0]+diff] #ods other option name,
     line[per_student_SCH_EST_cost[1]]=fullstring[per_student_SCH_EST_cost[0]+diff] #school estimated cost per student
-    line[per_student_cost[1]]=generate_cost(fullstring,ODS_provider_fees[0]+diff) #cost per student estimated using the cost breakdown
+    for k in list(range(5)):
+        line[per_student_cost[1]+k]=fullstring[ODS_provider_fees[0]+diff+k] #cost per student estimated using the cost breakdown
+
     return line
--- qual.py
+++ qual.py
@@ -7,8 +7,15 @@ print(checkvariable)
 
 with open('outdoor.csv',newline='') as f:
     reader = csv.reader(f)
-    for k in list(range(4)):
+    skipnum=int(input('choose row number: '))
+    for k in list(range(skipnum)):
         row=next(reader)
 
-    row=scrape_data_into_list(row,SCHname[0])
-    print(str(row))
+
+    num=get_num_of_schools_row(row)
+    print('total schools: '+str(num))
+    for k in list(range(num)):
+        rowscraped=scrape_data_into_list(row,k+1)
+        print(str(rowscraped))
+
+    #print(str(row))
