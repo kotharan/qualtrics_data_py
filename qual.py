fe62843c9f487689a0b7c7a61befceb419039ee5
Anand S Kothari <kotharan@oregonstate.edu>
2018-04-14 01:22:45 -0700

    Prints Columns
    Prints the date and the total of Progress column

--- qual.py
+++ qual.py
@@ -1,9 +1,15 @@
 import csv
+import pandas as pd
 
-with open('outdoor.csv',newline='') as f:
-    reader = csv.reader(f)
-    row1 = next(reader) # gets the first line
-     # now do something here
-    # if first row is the header, then you can do one more next() to get the next row:
-    # row2 = next(f)
-print(row1)
+with open('outdoor.csv','rb') as file:
+    # reader = csv.reader(file)
+    # lines = [line for line in file]
+    # print(lines[25])
+
+
+    df = pd.read_csv(file)
+    saved_column = df.EndDate + "   " + df.StartDate #you can also use df['column_name']
+
+    print(saved_column)
+    a = list(df.Progress[2:].astype(int))
+    print(sum(a))
