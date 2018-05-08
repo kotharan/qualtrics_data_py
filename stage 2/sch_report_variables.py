4be54f46f69e7a52e06d405a357e1bc08bac35b0
Kale <thekaleman@users.noreply.github.com>
2018-05-08 12:16:47 -0700

    School report spreadsheet template This holds pretty much every important index in the file. Now we just have to import it into whatever main script is used. Some commented rows contained spaced out numbers, these are justlist indexes

---
+++ stage 2/sch_report_variables.py
@@ -0,0 +1,60 @@
+
+
+#this file contains cell names for different variables
+sch_name='B2'
+sch_short_name='D2'
+
+#section 2 contact info
+sec2_names=['School contact name','School contact email','School contact phone','District/ESD contact name','District/ESD contact email','District/ESD contact email']
+#                     0                      1                       2                           3                        4                         5
+sec2_ans=['C9','C10','C11','F9','F10','F11']
+
+#section 3 concepts
+sec3_names=['Soil,Water, Plants, & Animals','Role of timber, agriculture, and other natural resources in the economy of this state','The interrelationship of nature, natural resources, economic development and career opportunities in this state','The importance of this state’s environment and natural resources','The development of students’ leadership, critical thinking and decision-making skills']
+#                        0                                         1                                                                           2                                                                                                                                            3                                              4
+sec3_ans=['G18','G19','G20','G21','G22']
+
+#section 4 content ratings
+sec4_names=['Science','Social Science','Food & Agriculture','Forestry',r'Sustainability/Enviromental Ed','Education Arts','Language Arts','Math','Geography',r'STEM/STEAM','Visual and Permorming Arts',r'Physical/Health Ed',r'Other- Survival/ Camp skills']
+#               0              1                    2            3                     4                            5             6         7          8           9                10                             11                   12
+sec4_ans=['C27','C28','C29','C30','C31','C32','C33','F27','F28','F29','F30','F31','F32']
+#section 5 text input
+sec5_prompt='B35'
+sec5_ans='B36'
+#section 6 text input
+sec6_prompt='B38'
+sec6_ans='B39'
+#section 7 text input
+sec7_prompt='B41'
+sec7_ans='B42'
+#section 8 text input
+sec8_prompt='B44'
+sec8_ans='B45'
+#sec 9 check boxes
+sec9_prompt='B47'
+sec9_subprompts=['Project based Learning','Cooperative learning stategies','Service Learning','Interdisciplinary instruction','Inquiry-based instruction','Social Emotional learning','Socio scientific issues','Other (list)']
+#                       0                                 1                               2            3                              4                         5                                6                    7
+sec9_ans=['C48','C49','C50','C51','C52','C53','C54','C55']
+#sec 10 text input
+sec10_prompt='B58'
+sec10_ans='B59'
+#sec 11 text input
+sec11_prompt='B61'
+sec11_ans='B62'
+#sec 12 multiple text inputs
+sec12_names=['Higher scores on standardized measures of academic achievement in reading, writing, math, science and social studies.','Greater self-sufficiency and leadership skills','Fewer discipline and classroom management problems','Increased student engagement and pride in accomplishments','Greater proficiency in solving problems and thinking strategically.','Better application of systems thinking and increased ability to think creatively.','Improved communication skills and enhanced ability to work in group settings.','Greater enthusiasm for language arts, math, science and social studies','Increased knowledge and understanding of science content, concepts and processes.','Better ability to apply science and civic processes to real-world situations','Improved understanding of mathematical concepts and mastery of math skills.','Improved language arts skills.','Better comprehension of social studies content.','Accessibility to students of all abilities and learning styles']
+sec12_ans=['C65','C66','C67','C68','C69','C70','C72','C73','C74','C75','C76','C77','C78']
+#sec 13 multiple text inputs
+sec13_prompt=r'Please describe how your program serves historicaly underrepresented populations. What actions are you taking to make this an experience for all students to thrive? (Select and briefly describe all that apply.)'
+sec13_subprompts=['Rural','English language learners','Special education','Learners with disabilities','Low socio-economic','American Indian/Alaska Native','Asian','Native Hawaiian/Pacific Islander','Black/African American','Hispanic/Latino','Other (list)']
+#                   0                 1                         2                         3                        4                  5                        6                    7                            8                    9                    10
+sec13_ans=['C81','C82','C83','C84','C85','C86','C87','C88','C89','C90','C91']
+#sec 14 text input
+sec14_prompt='B93'
+sec14_ans='B94'
+#sec 15 text input
+sec15_prompt='B96'
+sec15_ans='B97'
+#sec 16 text input
+sec16_prompt='B99'
+sec16_ans='B100'
