7e4c76fc3c776f5c818c5215a4dca3a9bee680b4
Anand Kothari <kotharan@oregonstate.edu>
2018-07-26 19:39:19 -0700

    Data Sorted and displayed in single row till option num 12 in raw document

--- SummerWork/new.py
+++ SummerWork/new.py
@@ -12,10 +12,11 @@ from pandas import ExcelFile
 #----------------------------------------------------------
 
 #data = pd.Excelfile(SummerWork.xlsx)
-dfs = pd.read_excel(r'C:\Users\kotharan\Documents\GitHub\qualtrics_data_py\SummerWork\All_Reports\Adrian_SD_61_2017-2018_Outdoor_School_Report.xlsx', sheet_name=None)
+dfs = pd.read_excel(r'C:\Users\DELL\Documents\GitHub\qualtrics_data_py\SummerWork\All_Reports\Adrian_SD_61_2017-2018_Outdoor_School_Report.xlsx', sheet_name=None)
 
+# Book is the name of the workbook of the raw data
 
-book = xlrd.open_workbook(r'C:\Users\kotharan\Documents\GitHub\qualtrics_data_py\SummerWork\All_Reports\Adrian_SD_61_2017-2018_Outdoor_School_Report.xlsx')
+book = xlrd.open_workbook(r'C:\Users\DELL\Documents\GitHub\qualtrics_data_py\SummerWork\All_Reports\Adrian_SD_61_2017-2018_Outdoor_School_Report.xlsx')
 worksheet = book.sheet_by_name('Adrian School District #61')
 num_rows = worksheet.nrows # shows the number of rows in a sheets
 
@@ -73,12 +74,10 @@ worksheet1.write('H2', sheet1_Data[7].value )
 
 
 #This sheet will store data from option 3 to 16
-sheet2_Data = [first_sheet.cell(1,1)] # Redefining the same variable cell to use it for another worksheet
+sheet2_Data = [first_sheet.cell(17,6),first_sheet.cell(18,6),first_sheet.cell(19,6),first_sheet.cell(20,6),first_sheet.cell(21,6),first_sheet.cell(26,2),first_sheet.cell(27,2),first_sheet.cell(28,2),first_sheet.cell(29,2),first_sheet.cell(30,2),first_sheet.cell(31,2),first_sheet.cell(32,2),first_sheet.cell(26,5),first_sheet.cell(27,5),first_sheet.cell(28,5),first_sheet.cell(29,5),first_sheet.cell(30,5),first_sheet.cell(31,5),first_sheet.cell(34,1),first_sheet.cell(35,1),first_sheet.cell(37,1),first_sheet.cell(38,1),first_sheet.cell(40,1),first_sheet.cell(41,1),first_sheet.cell(43,1),first_sheet.cell(44,1),first_sheet.cell(46,1),first_sheet.cell(47,2),first_sheet.cell(48,2),first_sheet.cell(49,2),first_sheet.cell(50,2),first_sheet.cell(51,2),first_sheet.cell(52,2),first_sheet.cell(53,2),first_sheet.cell(55,3),first_sheet.cell(57,1),first_sheet.cell(58,1),first_sheet.cell(60,1),first_sheet.cell(61,1),first_sheet.cell(63,1),first_sheet.cell(64,1),first_sheet.cell(64,2),first_sheet.cell(65,1),first_sheet.cell(65,2),first_sheet.cell(66,1),first_sheet.cell(66,2),first_sheet.cell(67,1),first_sheet.cell(67,2),first_sheet.cell(68,1),first_sheet.cell(68,2),first_sheet.cell(69,1),first_sheet.cell(69,2),first_sheet.cell(70,1),first_sheet.cell(70,2),first_sheet.cell(71,1),first_sheet.cell(71,2),first_sheet.cell(72,1),first_sheet.cell(72,2),first_sheet.cell(73,1),first_sheet.cell(73,2),first_sheet.cell(74,1),first_sheet.cell(74,2),first_sheet.cell(75,1),first_sheet.cell(75,2),first_sheet.cell(76,1),first_sheet.cell(76,2),first_sheet.cell(77,1),first_sheet.cell(77,2)] # Redefining the same variable cell to use it for another worksheet
 
 worksheet2 = workbook.add_worksheet('Other Info') #Creating the second worksheet to write into
 #worksheet2.set_column(0, 1, 30)   # Columns [Start-column,End-column,Width] width set to 30.
-
-
 
 worksheet2.write('A2', "School Name")      #Print Scool Name
 worksheet2.write('B2', "District Name" )
@@ -86,7 +85,6 @@ worksheet2.write('A3', sheet1_Data[0].value )        #Taking values from our doc
 worksheet2.write('B3', sheet1_Data[1].value )
 
 cell_format = workbook.add_format({'color':'red'})
-
 
 worksheet2.write('C1', "Topic/ Concepts :                                                      " ,cell_format)
 
@@ -96,8 +94,130 @@ worksheet2.write('E2', "The interrelationship of nature, natural resources, econ
 worksheet2.write('F2', "The importance of this state’s environment and natural resources" )
 worksheet2.write('G2', "The development of students’ leadership, critical thinking and decision-making skills" )
 
+worksheet2.write('C3', sheet2_Data[0].value)
+worksheet2.write('D3', sheet2_Data[1].value)
+worksheet2.write('E3', sheet2_Data[2].value)
+worksheet2.write('F3', sheet2_Data[3].value)
+worksheet2.write('G3', sheet2_Data[4].value)
+
+worksheet2.write('H1', "Program Content :                                                      " ,cell_format)
+
+worksheet2.write('H2',"Science" )
+worksheet2.write('I2',"Socail Science" )
+worksheet2.write('J2',"Food & Agriculture" )
+worksheet2.write('K2',"Forestry" )
+worksheet2.write('L2',"Sustainability/Enviromental Ed" )
+worksheet2.write('M2',"Education Arts" )
+worksheet2.write('N2',"Language Arts" )
+worksheet2.write('O2',"Math" )
+worksheet2.write('P2',"Geography" )
+worksheet2.write('Q2',"STEM/STEAM" )
+worksheet2.write('R2',"Visual and Performing Arts" )
+worksheet2.write('S2',"Physical/Health Ed" )
+worksheet2.write('T2',"Other" )
 
 
-#Continuew from adding values  of the option 6 
+worksheet2.write('H3', sheet2_Data[5].value)
+worksheet2.write('I3', sheet2_Data[6].value)
+worksheet2.write('J3', sheet2_Data[7].value)
+worksheet2.write('K3', sheet2_Data[8].value)
+worksheet2.write('L3', sheet2_Data[9].value)
+worksheet2.write('M3', sheet2_Data[10].value)
+worksheet2.write('N3', sheet2_Data[11].value)
+worksheet2.write('O3', sheet2_Data[12].value)
+worksheet2.write('P3', sheet2_Data[13].value)
+worksheet2.write('Q3', sheet2_Data[14].value)
+worksheet2.write('R3', sheet2_Data[15].value)
+worksheet2.write('S3', sheet2_Data[16].value)
+worksheet2.write('T3', sheet2_Data[17].value)
+
+
+worksheet2.write('U1', sheet2_Data[18].value, cell_format) # This is the question for option 5 from the main spreadsheet
+worksheet2.write('U3', sheet2_Data[19].value) # This is the answer for option 5 from the main spreadsheet
+
+worksheet2.write('V1', sheet2_Data[20].value,cell_format) # This is the question for option 6 from the main spreadsheet
+worksheet2.write('V3', sheet2_Data[21].value) # This is the answer for option 6 from the main spreadsheet
+
+worksheet2.write('W1', sheet2_Data[22].value,cell_format) # This is the question for option 7 from the main spreadsheet
+worksheet2.write('W3', sheet2_Data[23].value) # This is the answer for option 7 from the main spreadsheet
+
+worksheet2.write('X1', sheet2_Data[24].value,cell_format) # This is the question for option 8 from the main spreadsheet
+worksheet2.write('X3', sheet2_Data[25].value) # This is the answer for option 8 from the main spreadsheet
+
+worksheet2.write('Y1', sheet2_Data[26].value,cell_format) # This is the question for option 9 from the main spreadsheet
+worksheet2.write('Y2', "Project based Learning") # This is the subpart for option 9 from the main spreadsheet
+worksheet2.write('Y3', sheet2_Data[27].value) # This is the answer for option 9 from the main spreadsheet
+
+worksheet2.write('Z2', "Cooperative learning stategies") # This is the subpart for option 9 from the main spreadsheet
+worksheet2.write('Z3', sheet2_Data[28].value) # This is the answer for option 9 from the main spreadsheet
+
+worksheet2.write('AA2', "Service Learning") # This is the subpart for option 9 from the main spreadsheet
+worksheet2.write('AA3', sheet2_Data[29].value) # This is the answer for option 9 from the main spreadsheet
+
+worksheet2.write('AB2', "Interdisciplinary instruction") # This is the subpart for option 9 from the main spreadsheet
+worksheet2.write('AB3', sheet2_Data[30].value) # This is the answer for option 9 from the main spreadsheet
+
+worksheet2.write('AC2', "Inquiry-based instruction") # This is the subpart for option 9 from the main spreadsheet
+worksheet2.write('AC3', sheet2_Data[31].value) # This is the answer for option 9 from the main spreadsheet
+
+worksheet2.write('AD2', "Social Emotional learning") # This is the subpart for option 9 from the main spreadsheet
+worksheet2.write('AD3', sheet2_Data[32].value) # This is the answer for option 9 from the main spreadsheet
+
+worksheet2.write('AE2', "Socio scientific issues") # This is the subpart for option 9 from the main spreadsheet
+worksheet2.write('AE3', sheet2_Data[33].value) # This is the answer for option 9 from the main spreadsheet
+
+worksheet2.write('AF2', "Other (list)") # This is the subpart for option 9 from the main spreadsheet
+worksheet2.write('AF3', sheet2_Data[34].value) # This is the answer for option 9 from the main spreadsheet
+
+worksheet2.write('AG1',sheet2_Data[35].value,cell_format ) # This is the question for option 10 from the main spreadsheet
+worksheet2.write('AG3', sheet2_Data[36].value) # This is the answer for option 10 from the main spreadsheet
+
+worksheet2.write('AH1',sheet2_Data[37].value,cell_format ) # This is the question for option 11 from the main spreadsheet
+worksheet2.write('AH3', sheet2_Data[38].value) # This is the answer for option 11 from the main spreadsheet
+
+worksheet2.write('AI1', sheet2_Data[39].value,cell_format) # This is the question for option 12 from the main spreadsheet
+worksheet2.write('AI2', sheet2_Data[40].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AI3', sheet2_Data[41].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AJ2', sheet2_Data[42].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AJ3', sheet2_Data[43].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AK2', sheet2_Data[44].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AK3', sheet2_Data[45].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AL2', sheet2_Data[46].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AL3', sheet2_Data[47].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AM2', sheet2_Data[48].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AM3', sheet2_Data[49].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AN2', sheet2_Data[50].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AN3', sheet2_Data[51].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AO2', sheet2_Data[52].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AO3', sheet2_Data[53].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AP2', sheet2_Data[54].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AP3', sheet2_Data[55].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AQ2', sheet2_Data[56].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AQ3', sheet2_Data[57].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AR2', sheet2_Data[58].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AR3', sheet2_Data[59].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AS2', sheet2_Data[60].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AS3', sheet2_Data[61].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AT2', sheet2_Data[62].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AT3', sheet2_Data[63].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AU2', sheet2_Data[64].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AU3', sheet2_Data[65].value) # This is the answer for option 12 from the main spreadsheet
+
+worksheet2.write('AV2', sheet2_Data[66].value) # This is the subpart for option 12 from the main spreadsheet
+worksheet2.write('AV3', sheet2_Data[67].value) # This is the answer for option 12 from the main spreadsheet
+
+#Continue from adding values  of the option 13
 
 workbook.close()
