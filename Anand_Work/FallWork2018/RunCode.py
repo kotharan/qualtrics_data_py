388876bd18f34794a3ba90d8c64edddb395ff3ff
Anand Kothari <kotharan@oregonstate.edu>
2018-11-03 11:53:01 -0700

    Done with section 4 need to make changes in comments

--- Anand_Work/FallWork2018/RunCode.py
+++ Anand_Work/FallWork2018/RunCode.py
@@ -15,7 +15,7 @@ worksheet = inputbook.sheet_by_index(0)
 output_Workbook = xlsxwriter.Workbook(dir_path+'\OUTPUT.xlsx') #Creating new output_Workbook to write into
 outWorkSheet = output_Workbook.add_worksheet('Provider Suvery Data') #Creating new worksheet to write into
 
-# Used only the bold ones to see the schoolnames and their other data other than the first school data
+# Used only the bold ones to see the facilitynames and their other data other than the first facility data
 italic = output_Workbook.add_format({'italic': True}) #To do cell text formatting
 bold = output_Workbook.add_format({'bold': True})
 
@@ -38,10 +38,10 @@ Functions =>
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 Section5(Parameters)              ::>     This starts reading from the 3rd row of the input xlrd data and keeps on looping till the 62th column.
-                                Then it checks for a cell which speciefies the number of school that district has and loops to each school's
-                                data and prints in under the main district row. After it is done looping through all the schools it also creates an
-                                empty row so the other district data is easily identifyable. It also applies bold cell format to each school data after
-                                the first main district row data. [It might be hard to understand by this descriptiton but it makes more sense when your compare
+                                Then it checks for a cell which speciefies the number of facilities that ? has and loops to each facility's
+                                data and prints in under the main ? row. After it is done looping through all the facilitys it also creates an
+                                empty row so the other ? data is easily identifyable. It also applies bold cell format to each facility data after
+                                the first main ? row data. [It might be hard to understand by this descriptiton but it makes more sense when your compare
                                 the input and output file]
 
 
@@ -83,7 +83,7 @@ def Section5(Read_Start_from_row, till_row , Read_Start_from_col , till_col , Wr
                     Write_Start_from_col = 39
                     Write_Start_from_row += 1
 
-        Write_Start_from_col=0                                                         # Start from the col 0 after you print data of a district
+        Write_Start_from_col=0                                                         # Start from the col 0 after you print data of a ?
         Write_Start_from_row+=1                                                        # Go to the next row as well
 
 def Section4(Read_Start_from_row, till_row , Read_Start_from_col , till_col , Write_Start_from_row , Write_Start_from_col ):
@@ -95,27 +95,27 @@ def Section4(Read_Start_from_row, till_row , Read_Start_from_col , till_col , Wr
             outWorkSheet.write(Write_Start_from_row,Write_Start_from_col,cell_obj)     # Write the cell_obj in OUTPUT file
             Write_Start_from_col +=1
         
-        # if(worksheet.cell(row, 19).value == ""):
-        #     pass
-        # elif (worksheet.cell(row, 19).value > 1):                                    # Loop for Section 3B in qualtrics. This checks if there are more than one facility and loops to each facility data if there is
-        #     num_of_facilities = int(worksheet.cell(row, 19).value)                   # Stores the Number of Facilities 
-        #     Write_Start_from_col=49                                                  # If there is more than one facility print the next facility data below the first facility data
-        #     Write_Start_from_row+=1
-        #     Facility_Row_End = 48 + (10 * num_of_facilities)
+        if(worksheet.cell(row, 19).value == ""):
+            pass
+        elif (worksheet.cell(row, 19).value > 1):                                    # Loop for Section 3B in qualtrics. This checks if there are more than one facility and loops to each facility data if there is
+            num_of_facilities = int(worksheet.cell(row, 19).value)                   # Stores the Number of Facilities 
+            Write_Start_from_col=49                                                  # If there is more than one facility print the next facility data below the first facility data
+            Write_Start_from_row+=1
+            Facility_Row_End = 138 + (21 * num_of_facilities)                       # 138 is 1 minus of starting read col 
 
-        #     for col in range(49,Facility_Row_End):                                 # This is 29 times num_of_facilities of facility because there are 29 questions for each facility
-        #         cell_data = worksheet.cell(row, col).value                           # Get cell object by row, col
-        #         outWorkSheet.write(Write_Start_from_row,Write_Start_from_col,cell_data,bold)
-        #         Write_Start_from_col +=1
+            for col in range(160,Facility_Row_End):                                #160 is second loop start data # This is 21 times num_of_facilities of facility because there are 21 questions for each facility
+                cell_data = worksheet.cell(row, col).value                           # Get cell object by row, col
+                outWorkSheet.write(Write_Start_from_row,Write_Start_from_col,cell_data,bold)
+                Write_Start_from_col +=1
 
-        #         if Write_Start_from_col == 49:
-        #             Write_Start_from_col = 39
-        #             Write_Start_from_row += 1
+                if Write_Start_from_col == 70:                                      # 70 is end col of 1st loop
+                    Write_Start_from_col = 49
+                    Write_Start_from_row += 1
 
-        Write_Start_from_col=49                                                         # Start from the col 0 after you print data of a district
+        Write_Start_from_col=49                                                        # Start from the col 49 after you print data of a facility
         Write_Start_from_row+=1                                                        # Go to the next row as well
 
 Section5(3,worksheet.nrows,0,49,1,0)                                                   # Calling then function Section5
-Section4(3,worksheet.nrows,139,160,1,49)                                                   # Calling then function Section5
+Section4(3,worksheet.nrows,139,160,1,49)                                               # Calling then function Section5
 
 output_Workbook.close()                                                                # Close the workbook when done
