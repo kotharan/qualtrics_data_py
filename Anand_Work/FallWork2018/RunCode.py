7ac14763f350db4b97548b0f268f16a976ee1d32
Anand Kothari <kotharan@oregonstate.edu>
2018-11-08 22:25:34 -0800

    Section 6 Added

--- Anand_Work/FallWork2018/RunCode.py
+++ Anand_Work/FallWork2018/RunCode.py
@@ -153,10 +153,38 @@ def Section5(Read_Start_from_row, till_row , Read_Start_from_col , till_col , Wr
         Write_Start_from_col=70                                                        # Start from the col 49 after you print data of a facility
         Write_Start_from_row+=1                                                        # Go to the next row as well
 
+def Section6(Read_Start_from_row, till_row , Read_Start_from_col , till_col , Write_Start_from_row , Write_Start_from_col ):
+            # Read of row start from the data file                             # Writing in the OUTPUT file
+
+    for row in range(Read_Start_from_row , till_row):
+        for col in range(Read_Start_from_col, till_col):
+            cell_obj = worksheet.cell(row, col).value                                # Get cell object by row, col
+            outWorkSheet.write(Write_Start_from_row,Write_Start_from_col,cell_obj)     # Write the cell_obj in OUTPUT file
+            Write_Start_from_col +=1
+        
+        if(worksheet.cell(row, 19).value == ""):
+            pass
+        elif (worksheet.cell(row, 19).value > 1):                                    # Loop for Section 3B in qualtrics. This checks if there are more than one facility and loops to each facility data if there is
+            num_of_facilities = int(worksheet.cell(row, 19).value)                   # Stores the Number of Facilities 
+            Write_Start_from_col=84                                                  # If there is more than one facility print the next facility data below the first facility data
+            Write_Start_from_row+=1
+            Facility_Row_End = 1735 + (10 * num_of_facilities)                       # 139 of starting read col 
+
+            for col in range(1745,Facility_Row_End):                                #1745 is second loop start data # This is 21 times num_of_facilities of facility because there are 21 questions for each facility
+                cell_data = worksheet.cell(row, col).value                           # Get cell object by row, col
+                outWorkSheet.write(Write_Start_from_row,Write_Start_from_col,cell_data,bold)
+                Write_Start_from_col +=1
+
+                if Write_Start_from_col == 94:                                      # 94 is end col of 1st loop
+                    Write_Start_from_col = 84
+                    Write_Start_from_row += 1
+
+        Write_Start_from_col=84                                                        # Start from the col 49 after you print data of a facility
+        Write_Start_from_row+=1                                                        # Go to the next row as well
 
 Section3b(3,worksheet.nrows,0,49,1,0)                                                   # Calling then function Section3b
 Section4(3,worksheet.nrows,139,160,1,49)                                               # Calling then function Section4
 Section5(3,worksheet.nrows,349,363,1,70)
-
+Section6(3,worksheet.nrows,1735,1745,1,84)
 
 output_Workbook.close()                                                                # Close the workbook when done
